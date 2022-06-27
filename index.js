06fd6c96f7c0ff7d7990165ef32dbf9b53d2dea3
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-27 11:48:03 -0500

    First 2 functions complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+}
+
+function calculatesFarePrice(start, destination) {
+
+}
